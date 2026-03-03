e3c8cb7a3f74584483d90cff0f3e89535565035c
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-03-03 18:00:51 +0000

    feat(dashboard): implementacao da tabela kanban (to do/doing/done)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ITask } from './models/task.model'; // Importa a sua interface
+import { TaskCard } from './components/task-card/task-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [TaskCard], // Mantém o RouterOutlet para as rotas futuras
+  templateUrl: './app.html', // O Angular v18 usa 'app.html' em vez de 'app.component.html'
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('GestorDeTarefas');
+  // Mantemos o signal que o Angular criou, mas com o nome do seu projeto
+  protected readonly title = signal('TaskWave');
+
+  // Adicionamos a sua lista de tarefas (Mock Data)
+  tasks: ITask[] = [
+    {
+      id: 1,
+      title: 'Configurar Repositório',
+      description: 'Lincar o projeto local ao GitHub',
+      category: 'Work',
+      status: 'Done',
+      dueDate: new Date(),
+      createdAt: new Date()
+    },
+    {
+      id: 2,
+      title: 'Desenhar Componente de Card',
+      description: 'Criar o HTML/CSS do cartão de tarefa',
+      category: 'Urgent',
+      status: 'To Do',
+      dueDate: new Date(),
+      createdAt: new Date()
+    }
+  ];
 }
