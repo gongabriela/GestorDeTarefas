ff5c2732cbf0a772826d5470d8e5c03f2152b2f9
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-03-06 15:04:38 +0000

    feat(task-details): implementando estrutura e logica do task-details

--- src/app/pages/task-details/task-details.ts
+++ src/app/pages/task-details/task-details.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule, Location } from '@angular/common';
+import { TaskService } from '../../services/task-service';
+import { ITask } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-details',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './task-details.html',
-  styleUrl: './task-details.css',
+  styleUrl: './task-details.css'
 })
-export class TaskDetails {}
+export class TaskDetails implements OnInit {
+  task?: ITask;
+
+  constructor(
+    private route: ActivatedRoute,
+    private taskService: TaskService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.task = this.taskService.getTaskById(id);
+    }
+  }
+  //APAGAR transformar isso em pipe pq tb usamos esse mesmo codigo no task-item
+  get categoryIcon(): string {
+    const cat = this.task?.category.toLowerCase() || '';
+    switch (cat) {
+      case 'personal': return 'fa-user';
+      case 'study': return 'fa-book-open';
+      case 'work': return 'fa-briefcase';
+      case 'urgent': return 'fa-bolt';
+      default: return 'fa-list-check';
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  deleteTask(): void {
+    if (this.task && confirm('Are you sure you want to delete this task?')) {
+      this.taskService.deleteTask(this.task.id);
+      this.goBack();
+    }
+  }
+}
