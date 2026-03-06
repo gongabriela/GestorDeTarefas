ac307229fe5c9614f8214428efffb0e785fb720d
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-03-06 11:47:12 +0000

    feat(edit-task): adicionando o router ao task-card para finalizar o roteamento

--- src/app/components/task-card/task-card.ts
+++ src/app/components/task-card/task-card.ts
@@ -1,22 +1,18 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { ITask } from '../../models/task.model';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-task-card',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink ],
   templateUrl: './task-card.html',
   styleUrl: './task-card.css',
 })
 export class TaskCard {
-  @Input({ required: true }) task!: ITask; // Recebe a tarefa como input para exibir os detalhes
-  
-  @Output () editTask = new EventEmitter<ITask>(); // Emite um evento quando a tarefa é editada
-  onEditTask(task: ITask) : void {
-    this.editTask.emit(task);
-  }
+  @Input({ required: true }) task!: ITask;
 
-  @Output () deleteTask = new EventEmitter<ITask>(); // Emite um evento quando a tarefa é deletada
+  @Output () deleteTask = new EventEmitter<ITask>();
   onDeleteTask(task: ITask) : void {
     this.deleteTask.emit(task);
   }
