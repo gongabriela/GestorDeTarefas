c4eda6e33151f49683f6ff3b24eee59acc6b0ea1
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-03-07 16:50:31 +0000

    feat: criando tipagem para os filtros por categoria

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -2,8 +2,10 @@ export interface ITask {
   id: number;
   title: string;
   description: string;
-  category: 'Work' | 'Personal' | 'Study' | 'Urgent'; // Categorias fixas para facilitar filtros
-  status: 'To Do' | 'Doing' | 'Done';              // Os blocos do seu Kanban
+  category: 'Work' | 'Personal' | 'Study' | 'Urgent';
+  status: 'To Do' | 'Doing' | 'Done';
   dueDate: Date;
   createdAt: Date;
 }
+
+export type CategoryFilter = ITask['category'] | 'All';
